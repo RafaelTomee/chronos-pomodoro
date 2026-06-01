51f261309d2dc40c910c3ea5d5c7feef3d2b608c
Rafael Tomé <rafaeltome2004@gmail.com>
2026-06-01 17:32:01 -0300

    props e children como componentes/parametros

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Heading } from "./components/Heading";
 export function App() {
   return (
     <>
-      <Heading />
+      <Heading attr={123} attr2="string">Olá mundo children</Heading>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, harum. Voluptatibus atque mollitia quidem debitis quia nostrum culpa nulla eos sint excepturi doloribus pariatur, dicta perspiciatis, distinctio deserunt magni doloremque.</p>
     </>
   );
