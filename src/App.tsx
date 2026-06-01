f1694e87f147e400c5cf4c67b94885c58bdcdf60
Rafael Tomé <rafaeltome2004@gmail.com>
2026-06-01 18:06:00 -0300

    Mudei a tipagem do children para ReactNode e terminei o componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
-import "./styles/theme.css";
-import "./styles/global.css";
+import './styles/theme.css';
+import './styles/global.css';
 
-import { Heading } from "./components/Heading";
+import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading attr={123} attr2="string">Olá mundo children</Heading>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, harum. Voluptatibus atque mollitia quidem debitis quia nostrum culpa nulla eos sint excepturi doloribus pariatur, dicta perspiciatis, distinctio deserunt magni doloremque.</p>
+      <Heading>Olá mundo children
+
+        <button> <TimerIcon /></button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, harum.
+        Voluptatibus atque mollitia quidem debitis quia nostrum culpa nulla eos
+        sint excepturi doloribus pariatur, dicta perspiciatis, distinctio
+        deserunt magni doloremque.
+      </p>
     </>
   );
 }
