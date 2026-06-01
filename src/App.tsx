d80f5fdba0474dc3825e74084e72d502041f7dc9
Rafael Tomé <rafaeltome2004@gmail.com>
2026-06-01 17:06:13 -0300

    Importando e usando arquivos de CSS Modules com escopo de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
+import { Heading } from "./components/Heading";
+
 export function App() {
   return (
     <>
-      <h1>Olá mundo (do App)</h1>
+      <Heading />
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo, harum. Voluptatibus atque mollitia quidem debitis quia nostrum culpa nulla eos sint excepturi doloribus pariatur, dicta perspiciatis, distinctio deserunt magni doloremque.</p>
     </>
   );
 }
