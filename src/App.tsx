bcda5b3d65492f56fb2a7610da2430a5ff8fe86f
Rafael Tomé <rafaeltome2004@gmail.com>
2026-06-05 04:09:58 -0300

    feat: footer feito

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Heading } from './components/heading/Heading';
-import { Home } from './components/Home';
+import { Home } from './components/home/Home';
+import { Footer } from './components/footer/Footer';
 
 export function App() {
   return (
     <>
       <Heading>Chronos</Heading>
       <Home minutos={25} />
+      <Footer/>
     </>
   );
 }
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      <p>Entenda a técnica pomodoro 🍅</p>
+      <p>Chronos Pomodoro © 2026 - Feito com 💚</p>
+    </div>
+  );
+}
